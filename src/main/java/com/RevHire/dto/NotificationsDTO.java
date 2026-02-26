c9a6c9a7c40ce8af0e3f1b4582454203ba97b392
Damarkeswar <damarakeshwar789@gmail.com>
2026-02-26 14:41:01 +0530

    dto updated final -v1

--- src/main/java/com/RevHire/dto/NotificationsDTO.java
+++ src/main/java/com/RevHire/dto/NotificationsDTO.java
@@ -35,4 +35,6 @@ public class NotificationsDTO {
 
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    
 }
