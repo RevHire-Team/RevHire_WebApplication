3bdacf5905f48f79055bd0d76ee4318314b93904
Ameer <amir140302@gmail.com>
2026-02-26 15:34:12 +0530

    developed the job seeker profile management api controllers

--- src/main/java/com/RevHire/dto/NotificationRequestDTO.java
+++ src/main/java/com/RevHire/dto/NotificationRequestDTO.java
@@ -1,3 +1,20 @@
 package com.RevHire.dto;
 
-public class NotificationRequestDTO {}
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotificationRequestDTO {
+
+    private Long userId;
+    private String message;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
