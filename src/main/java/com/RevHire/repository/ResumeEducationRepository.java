dcc7486aaaf53615c4a614671023b3590a3f6287
AnumandlaManashwini <anumandlamanashwini@gmail.com>
2026-03-09 23:08:10 +0530

    ResumeEducationRepository updated

--- src/main/java/com/RevHire/repository/ResumeEducationRepository.java
+++ src/main/java/com/RevHire/repository/ResumeEducationRepository.java
@@ -4,12 +4,19 @@ import com.RevHire.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.RevHire.entity.ResumeEducation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
-public interface ResumeEducationRepository extends JpaRepository<ResumeEducation, Long> {
+public interface ResumeEducationRepository
+        extends JpaRepository<ResumeEducation, Long> {
 
-    List<ResumeEducation> findByResume_ResumeId(Long resumeId);
+    List<ResumeEducation> findByResumeResumeId(Long resumeId);
 
-    void deleteByResume(Resume resume);
+    @Transactional
+    @Modifying
+    void deleteByResumeResumeId(Long resumeId);
+
+
 }
