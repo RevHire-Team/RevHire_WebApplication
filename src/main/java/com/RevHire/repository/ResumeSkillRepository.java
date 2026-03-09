4a3463d2aec64402d15bbac9d598f71091053e71
AnumandlaManashwini <anumandlamanashwini@gmail.com>
2026-03-09 23:16:40 +0530

    ResumeSkillRepository  is updated

--- src/main/java/com/RevHire/repository/ResumeSkillRepository.java
+++ src/main/java/com/RevHire/repository/ResumeSkillRepository.java
@@ -1,17 +1,17 @@
 package com.RevHire.repository;
 
-import com.RevHire.entity.Resume;
-import com.RevHire.entity.ResumeExperience;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.RevHire.entity.ResumeSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
 public interface ResumeSkillRepository extends JpaRepository<ResumeSkill, Long> {
-    List<ResumeSkill> findByResume_ResumeId(Long resumeId);
 
-    void deleteByResume(Resume resume);
+    List<ResumeSkill> findByResumeResumeId(Long resumeId);
 
-    List<ResumeSkill> findByResume(Resume resume);
+    @Transactional
+    @Modifying
+    void deleteByResumeResumeId(Long resumeId);
 }
